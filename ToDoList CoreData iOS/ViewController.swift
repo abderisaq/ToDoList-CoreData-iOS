0c5fc92ffe93e038fb4e8af0fbe909ebe649d9a3
Abdul <tarabi@uw.edu>
2019-09-15 11:00:53 -0700

    Initial Commit

---
+++ ToDoList CoreData iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList CoreData iOS
+//
+//  Created by TN007 on 9/15/19.
+//  Copyright © 2019 Tarabi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
